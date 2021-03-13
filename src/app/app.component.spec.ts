c942d33abe1fa9a763c37e3f6815f0c30777a00d
0APOCALYPSE0 <giriaakash00@gmail.com>
2021-03-13 12:53:10 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('Testing AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -26,6 +26,39 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
+    // console.log(compiled.querySelector('.content span').textContent);
     expect(compiled.querySelector('.content span').textContent).toContain('test app is running!');
+  });
+
+  it('should render resources', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    // fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    console.log(compiled.querySelector('h2').textContent)
+    expect(compiled.querySelector('h2').textContent).toContain('Resources');
+  });
+
+  it('testing add method', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.add(6,4)).toEqual(10);
+  })
+
+  it('#clicked() should toggle #isOn', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.isOn).toBe(false, 'off at first');
+    app.clicked();
+    expect(app.isOn).toBe(true, 'on at first');
+    app.clicked();
+    expect(app.isOn).toBe(false, 'off after second click');
+  });
+
+  it('#clicked() should set #message to "is on"' ,() =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.message).toMatch(/is off/i, 'off at first');
+    app.clicked();
+    expect(app.message).toMatch(/is on/i, 'on after clicked');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+  isOn:boolean = false;
+
+  clicked(){
+    this.isOn = !this.isOn;
+  }
+
+  get message(){
+    return `The light is ${this.isOn ? 'On' : 'Off'}`;
+  }
+
+  add(num1:number, num2:number):number {
+    return num1+num2;
+  }
+
 }
